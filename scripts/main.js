1a5dbcac518c62e8c2649e8ded73ddabf4992300
ZakMos <z.zza@hotmail.com>
2018-05-31 16:01:09 +0200

    fix some bugs and change the id add to container

--- scripts/main.js
+++ scripts/main.js
@@ -45,36 +45,36 @@ function printResultScreen(title, message) {
 
 
 
-
-function guessingGame(){
+//
+//function guessingGame(){
 
     // Create a number to guess between 0 and 10
-  let numberToGuess = getRandomIntInclusive(1, 10); // 10
+  //let numberToGuess = getRandomIntInclusive(1, 10); // 10
 
   // Initialize a counter
-  let count = 1;
+  //let count = 1;
 
   // Make your fist guess
-  let guess = parseInt(document.getElementById('input1').value);
-  let containerElem = document.querySelector('#result1');
+  //let guess = parseInt(document.getElementById('input1').value);
+//  let containerElem = document.querySelector('#result1');
  // As long as the guess is incorrect or you have tried to guess less than 3 times, try to guess again
   // while(guess !== numberToGuess &&  count < 3){
-    if(isNaN(guess)){
-     return containerElem.innerHTML="What the hell do you think you're doing?!?!!! I asked for a number!"
-     }
+    //if(isNaN(guess)){
+    // return containerElem.innerHTML="What the hell do you think you're doing?!?!!! I asked for a number!"
+  //   }
 
-     else if(guess !== numberToGuess){
+    // else if(guess !== numberToGuess){
 
-        if(count > 3){
-          containerElem.innerHTML= `Sorry human, but you couldn't guess the number in less than three times. The number was ${numberToGuess}`;
-        }
-        else{
-        containerElem.innerHTML=`Wrong!!! Try Again! You have ${3 - count} attempts left`;
+      //  if(count > 3){
+        //  containerElem.innerHTML= `Sorry human, but you couldn't guess the number in less than three times. The number was ${numberToGuess}`;
+        //}
+        //else{
+        //containerElem.innerHTML=`Wrong!!! Try Again! You have ${3 - count} attempts left`;
 
-        }
+        //}
 
-        count +=1;
-     }
+        //count +=1;
+     //}
     //    else {
     //   // Try to guess a number
     //  return containerElem.innerHTML=`Wrong!!! Try Again! You have ${3 - count} attempts left`;
@@ -85,11 +85,11 @@ function guessingGame(){
   // }
 
   // If you guessed within 3 attempts...
-  else{
+  //else{
     // ... print the success message
-    return containerElem.innerHTML=  `Success: the number is indeed ${numberToGuess}. You got the number correctly after ${count} attempts.`;
-  }
-}
+  //  return containerElem.innerHTML=  `Success: the number is indeed ${numberToGuess}. You got the number correctly after ${count} attempts.`;
+  //}
+//}
 
 // function writeResult(string){
 //   document.getElementById('result1').innerText = string;
