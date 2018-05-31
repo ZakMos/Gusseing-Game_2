0b8022ff08976333a6d6650cac497af653331a83
majed187 <majedshrndan45@gmail.com>
2018-05-31 15:57:48 +0200

    delete  the extra files

--- scripts/main.js
+++ scripts/main.js
@@ -1,73 +1,47 @@
 function getRandomIntInclusive(min, max) {
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min + 1)) + min; //The maximum is inclusive and the minimum is inclusive
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min; //The maximum is inclusive and the minimum is inclusive
 }
 
-function guessingGame(){
 
-  // Create a number to guess between 0 and 10
-  let numberToGuess = getRandomIntInclusive(0, 10); // 10
+function printScreen(htmlString){
+    let containerElem = document.querySelector('#container');
 
-  // Initialize a counter
-  let count = 1;
+    containerElem.innerHTML = htmlString;
+}
 
-  // Make your fist guess
-  let guess = parseInt(document.getElementById('input1').value);
-  let containerElem = document.querySelector('#result1');
- // As long as the guess is incorrect or you have tried to guess less than 3 times, try to guess again
-  // while(guess !== numberToGuess &&  count < 3){
-    if(isNaN(guess)){
-     return containerElem.innerHTML="What the hell do you think you're doing?!?!!! I asked for a number!"
-     }
+    let numberToGuess = getRandomIntInclusive(0, 10); // 10
 
-     else if(guess !== numberToGuess){
-      
-        if(count > 3){
-          containerElem.innerHTML= `Sorry human, but you couldn't guess the number in less than three times. The number was ${numberToGuess}`;
+
+    let count = 1;
+
+    // Make your fist guess
+    let guess = parseInt(prompt('Guess a number between 1 - 10: (You have 3 attempts)'));
+
+    // As long as the guess is incorrect or you have tried to guess less than 3 times, try to guess again
+    while(guess !== numberToGuess &&  count < 3){
+        if(isNaN(guess)){
+            guess = parseInt(prompt('What the hell do you think you\'re doing?!?!!! I asked for a number!'));
+        } else {
+            // Try to guess a number
+            guess = parseInt(prompt(`Wrong!!! Try Again! You have ${3 - count}         attempts left`));
+            // Count the guess
+            count++;
         }
-        else{
-        containerElem.innerHTML=`Wrong!!! Try Again! You have ${3 - count} attempts left`;
-      
-        }
-        
-        count +=1;
-     }
-    //    else {
-    //   // Try to guess a number
-    //  return containerElem.innerHTML=`Wrong!!! Try Again! You have ${3 - count} attempts left`;
-     
-    //   // Count the guess
-    //   count++;
-    //  }
-  // }
+    }
 
-  // If you guessed within 3 attempts...
-  else{
+    // If you guessed within 3 attempts...
+    if(guess === numberToGuess && count <= 3){
     // ... print the success message
-    return containerElem.innerHTML=  `Success: the number is indeed ${numberToGuess}. You got the number correctly after ${count} attempts.`;
-  } 
+        return `Success: the number is indeed ${numberToGuess}. You got the number correctly after ${count} attempts.`;
+    } else {
+    // Otherwise print that the game is over
+        return `Sorry human, but you couldn't guess the number in less than three times. The number was ${numberToGuess}`;
+    }
 }
 
-// function writeResult(string){
-//   document.getElementById('result1').innerText = string;
-// }
-  // This is the better way to do it, however it's still not the most ideal way, I'll explain after the vacation.
-  //  document.getElementById('play').onclick = () => writeResult(guessingGame())
 
-
-  // function printScreen(htmlString){
-  //   let containerElem = document.querySelector('#result1');
-    
-  //   containerElem.innerHTML = htmlString;
-  // }
-  
-  // printScreen(`<button id="changeScreen">Change</button>`);
-  
-  // let buttonElem = document.querySelector('#play');
-  
-  // buttonElem.addEventListener('click', event => {
-  //   event.preventDefault();
-    
-  //  return printScreen(guessingGame());
-  // });
+function writeResult(string){
+    document.getElementById('result').innerText = string;
+}
